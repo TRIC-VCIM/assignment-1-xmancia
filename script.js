50f0b1f5320d689783554e61955e043ed8433a68
xmancia <s00944888@acad.tri-c.edu>
2020-09-02 03:56:39 +0000

    Assignment Complete

---
+++ script.js
@@ -0,0 +1,15 @@
+const message = prompt('Please type in a number between 1 and 100.');
+console.log(message);
+
+let input = 10;
+
+let subtract = input - 5;
+console.log(subtract);
+
+let multiply = input * 2;
+console.log(multiply);
+
+
+
+
+
